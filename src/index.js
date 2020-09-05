0eb627296381dee6578575e9297bc457e8735798
tzvaita <tzvaita@gmail.com>
2020-09-05 23:01:32 +0200

    Add API query function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+// eslint-disable-next-line no-unused-vars
 import _ from 'lodash';
 import './style.css';
+
+const mainCont = document.getElementById('content');
+
+fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=efc2c4c4facd952727e77dae6934241e', { mode: 'cors' })
+  .then((response) => response.json())
+  .then((response) => {
+    mainCont.innerHTML = response.main.temp;
+    console.log(response);
+  });
